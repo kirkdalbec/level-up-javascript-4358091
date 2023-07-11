d305299a7509eef097314932a8e7b7a425b4b446
Kirk Dalbec <57540977+kirkdalbec@users.noreply.github.com>
2023-07-11 19:20:26 +0000

    Lesson 07

--- start/01_07_classroom_attendance.js
+++ start/01_07_classroom_attendance.js
@@ -1,1 +1,21 @@
-// Write your code here
+function getStudents(classroom){
+  let { hasTeachingAssistant, classlist } = classroom;
+  let teacher, TeachingAssistant, students;
+
+  if(hasTeachingAssistant){
+    [teacher, TeachingAssistant, ...students] = classlist;
+  } else {
+    [teacher, ...students] = classlist;
+  }
+  return students;
+}
+
+console.log(getStudents({
+  hasTeachingAssistant: false,
+  classlist: ['Teacher', 'Student 01', 'Student 02', 'Student 03', 'Student 04']
+}));
+
+console.log(getStudents({
+  hasTeachingAssistant: true,
+  classlist: ['Teacher', 'Assistant', 'Student 05', 'Student 06', 'Student 07', 'Student 08']
+}));
